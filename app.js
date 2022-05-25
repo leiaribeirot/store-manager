487115455f89985e58a093466de4c7f61feb929a
Léia Ribeiro <leiaribeirot@gmail.com>
2022-05-25 18:41:21 -0400

    dev: importa rotas para o app

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const routers = require('./router');
 
 const app = express();
+app.use(express.json());
+app.use(routers);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
