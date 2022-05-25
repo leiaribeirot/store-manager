e5e043329c956da9afbd0f4b29ad4f73a4142aa6
Léia Ribeiro <leiaribeirot@gmail.com>
2022-05-25 17:52:49 -0400

    dev: cria function getProductById em Models

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -2,5 +2,6 @@ const connection = require('../db/connection');
 
 const getProductAll = () => connection.execute('SELECT * FROM products');
 
+const getProductById = (id) => connection.execute('SELECT * FROM products WHERE id = ?', [id]);
 
-module.exports = { getProductAll };
+module.exports = { getProductAll, getProductById };
