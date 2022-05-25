cfad2ce67cf30b95996b5518b25061f144ed5d98
Léia Ribeiro <leiaribeirot@gmail.com>
2022-05-25 18:32:54 -0400

    dev: router get productAll

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,1 +1,12 @@
-// Renomeie esse arquivo
+const express = require('express');
+
+const router = express.Router();
+
+const productService = require('../services/ProductService');
+
+router.get('/', async (_req, res) => {
+    const products = productService.getServiceAll();
+    res.status(200).json(products);
+});
+
+module.exports = router;
