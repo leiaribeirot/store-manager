e3b448a12db4d0f042d7ede760a18d87442e65b7
Léia Ribeiro <leiaribeirot@gmail.com>
2022-05-25 22:25:44 -0400

    dev: cira function getSalesById

--- models/SalesModel.js
+++ models/SalesModel.js
@@ -4,6 +4,11 @@ const getSalestAll = () => connection.execute(
     'SELECT * FROM sales_products ORDER BY sale_id, product_id',
 );
 
+const getSalesById = (id) => connection.execute(
+    'SELECT * FROM sales_products WHERE sales_id = ?', [id],
+);
+
 module.exports = {
     getSalestAll,
+    getSalesById,
 };
