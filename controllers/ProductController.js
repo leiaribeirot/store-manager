8b3d065b9f2548e84c2ee3730af115e88933febf
Léia Ribeiro <leiaribeirot@gmail.com>
2022-05-25 22:08:23 -0400

    dev: cria router product controller by id

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -9,4 +9,15 @@ router.get('/', async (_req, res) => {
     res.status(200).json(products);
 });
 
+router.get('/:id', async (req, res, _next) => {
+    const { id } = req.params;
+
+    const [product] = await productService.getServiceById(id);
+
+    if (!product) {
+        return res.status(404).json({ message: 'Product not found' });
+    }
+    return res.status(200).json(product);
+});
+
 module.exports = router;
