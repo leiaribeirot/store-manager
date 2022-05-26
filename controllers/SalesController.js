0b0f8aa6d19cb4f08181b6c150709f75e64cca23
Léia Ribeiro <leiaribeirot@gmail.com>
2022-05-25 23:17:25 -0400

    dev: cira router get salesById em controlles

--- controllers/SalesController.js
+++ controllers/SalesController.js
@@ -9,4 +9,13 @@ router.get('/', async (req, res) => {
     res.status(200).json(sales);
 });
 
+router.get('/:id', async (req, res, _next) => {
+    const { id } = req.params;
+    const [sales] = salesService.getSalesById(id);
+    if (!sales) {
+        return res.status(404).json({ message: 'Sale not found' });
+    }
+    return res.status(200).json(sales);
+});
+
 module.exports = router;
